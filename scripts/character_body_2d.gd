48cc868234d8b1f5a3657f4391da217724afc30e
jordonbigelow <jordon.bigelow.92@gmail.com>
2025-08-25 22:49:29 -0600

    added double jump mechanic

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
+const DOUBLE_JUMP_AMOUNT = 1
+var player_jump_amount = 0
+var can_player_double_jump: bool = true
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +16,16 @@ func _physics_process(delta: float) -> void:
     
   if is_on_wall() and Input.is_action_just_pressed("jump"):
     velocity.y = JUMP_VELOCITY
+  
+  if not is_on_floor() and Input.is_action_just_pressed("jump"):
+    if player_jump_amount < DOUBLE_JUMP_AMOUNT:
+      player_jump_amount += 1
+      
+      if can_player_double_jump:
+        velocity.y = JUMP_VELOCITY  
+  
+  if is_on_floor():
+    player_jump_amount = 0    
     
   if not is_on_floor():
     velocity += get_gravity() * delta
