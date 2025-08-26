46f1110f0b98be0b0db2eff40cda655cb9142144
jordonbigelow <jordon.bigelow.92@gmail.com>
2025-08-25 20:50:05 -0600

    added sprite flipping depending on the direction the player is facing

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -250.0
 
 
 func _physics_process(delta: float) -> void:
+  if Input.is_action_just_pressed("move_left"):
+    $Sprite2D.flip_h = true
+  if Input.is_action_just_pressed("move_right"):
+    $Sprite2D.flip_h = false
+    
   # Add the gravity.
   if not is_on_floor():
     velocity += get_gravity() * delta
