dd1d945690f43b618403345bd86aaaea8349590b
jordonbigelow <jordon.bigelow.92@gmail.com>
2025-08-25 21:32:59 -0600

    added wall jumping

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -11,16 +11,16 @@ func _physics_process(delta: float) -> void:
   if Input.is_action_just_pressed("move_right"):
     $Sprite2D.flip_h = false
     
-  # Add the gravity.
+  if is_on_wall() and Input.is_action_just_pressed("jump"):
+    velocity.y = JUMP_VELOCITY
+    print("you're on the wall")
+    
   if not is_on_floor():
     velocity += get_gravity() * delta
 
-  # Handle jump.
   if Input.is_action_just_pressed("jump") and is_on_floor():
     velocity.y = JUMP_VELOCITY
   
-  # Get the input direction and handle the movement/deceleration.
-  # As good practice, you should replace UI actions with custom gameplay actions.
   var direction := Input.get_axis("move_left", "move_right")
   if direction:
     velocity.x = direction * SPEED
@@ -35,6 +35,7 @@ func _physics_process(delta: float) -> void:
 # Perhaps find a different signal to use???
 func _on_biscuit_tree_exiting() -> void:
   print("You collected the Biscuit!")
+  
 
 func _on_kill_zone_body_entered(body: Node2D) -> void:
   print("You Dead, bruh!")
