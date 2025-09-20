ac29b6fdaf230898e9e927aa9f365c198ecd8e7a
jordonbigelow <the-music-dude@hotmail.com>
2025-09-20 10:13:22 -0600

    added connections to signals for sound effects

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var starting_screen: PackedScene = load("res://scenes/starting_screen.tscn")
+@onready var audio_player = $Player.get_node("AudioStreamPlayer2D")
+@onready var biscuit_collect_sound = load("res://assets/sound/biscuit_collect.wav")
+@onready var enemy_killed_sound = load("res://assets/sound/enemy_squished.wav")
+@onready var player_jump_sound = load("res://assets/sound/jump.wav")
 
 
 func _process(delta: float) -> void:
@@ -17,3 +21,17 @@ func _on_back_button_pressed() -> void:
 
 func _on_game_won_timer_timeout() -> void:
 	get_tree().change_scene_to_packed(starting_screen)
+
+
+func _on_enemy_killed() -> void:
+	audio_player.set_stream(enemy_killed_sound)
+	audio_player.play()
+
+
+func _on_audio_stream_player_2d_finished() -> void:
+	audio_player.set_stream(player_jump_sound)
+
+
+func _on_biscuit_player_collected(body: Node2D) -> void:
+	audio_player.set_stream(biscuit_collect_sound)
+	audio_player.play()
