c75fa0364b44d9cf32a7ec218514f73152df5a1c
jordonbigelow <the-music-dude@hotmail.com>
2025-09-20 20:22:24 -0600

    removed any get_node()/$Node calls inside of the _process callback

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,6 +6,12 @@ signal killed
 const SPEED = 45.0
 const JUMP_VELOCITY = -250.0
 
+@onready var kill_spot := $DeathDetector
+@onready var enemy_spirte := $Sprite2D
+@onready var ledge_detector := $LedgeDetector
+@onready var left_side_player_detector := $LeftSidePlayerDetector
+@onready var right_side_player_detector := $RightSidePlayerDetector
+
 var direction := -1
 
 
@@ -13,22 +19,22 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if $DeathDetector.is_colliding():
+	if kill_spot.is_colliding():
 		emit_signal("killed")
 		queue_free()
 	
-	if not $LedgeDetector.is_colliding():
+	if not ledge_detector.is_colliding():
 		direction *= -1
 		
-	if $LeftSidePlayerDetector.is_colliding():
+	if left_side_player_detector.is_colliding():
 		player_hit.emit()
-	elif $RightSidePlayerDetector.is_colliding():
+	elif right_side_player_detector.is_colliding():
 		player_hit.emit()
 		
 	if direction < 0:
-		$Sprite2D.flip_h = true
+		enemy_spirte.flip_h = true
 	if direction > 0:
-		$Sprite2D.flip_h = false
+		enemy_spirte.flip_h = false
 		
 	if direction:
 		velocity.x = direction * SPEED
