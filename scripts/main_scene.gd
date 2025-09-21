9938dc41bf1f7f4c8f5f798d3bdba10fc9d57016
jordonbigelow <the-music-dude@hotmail.com>
2025-09-20 20:52:45 -0600

    moved listeners and respawn timer to game manager

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -8,6 +8,10 @@ extends Node2D
 @onready var button_clicked_sound = load("res://assets/sound/button_click.wav")
 @onready var back_button: Button = $Player/Camera2D/HUD/BackButton
 @onready var game_restart_timer: Timer = $GameWonTimer
+@onready var respawn_timer := $RespawnTimer
+@onready var player_health_hud := $Player/Camera2D/HUD/HealthComponent
+@onready var heart_count_label := $Player/Camera2D/HUD/HealthComponent/HeartCount
+@onready var heart_count: int = 3
 
 
 func _process(_delta: float) -> void:
@@ -42,3 +46,21 @@ func _on_biscuit_player_collected(_body: Node2D) -> void:
 	audio_player.set_stream(biscuit_collect_sound)
 	audio_player.play()
 	game_restart_timer.start()
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Engine.time_scale = 0.5
+		respawn_timer.start()
+
+
+func _on_enemy_player_hit() -> void:
+	if heart_count > 0:
+		heart_count -= 1
+		player_health_hud.get_child(heart_count).hide()
+		heart_count_label.text = " x " + str(heart_count)
+
+	if heart_count <= 0:
+		if respawn_timer.time_left <= 0:
+			Engine.time_scale = 0.25
+			respawn_timer.start()
