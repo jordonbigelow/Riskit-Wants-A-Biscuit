272ac7f54ef7ada7f9aad08a2b0b8ab59ed89578
jordonbigelow <the-music-dude@hotmail.com>
2025-09-20 20:10:32 -0600

    refactored code to not have any get_node()/$Node calls inside of the _process() callback

--- scripts/starting_screen.gd
+++ scripts/starting_screen.gd
@@ -2,9 +2,10 @@ extends Control
 
 @onready var starting_scene: PackedScene = load("res://scenes/main_scene.tscn")
 @onready var guide_scene: PackedScene = load("res://scenes/guide_screen.tscn")
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_play_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
+	audio_player.play()
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_packed(starting_scene)
 
@@ -14,6 +15,6 @@ func _on_settings_button_pressed() -> void:
 
 
 func _on_guide_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
+	audio_player.play()
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_packed(guide_scene)
