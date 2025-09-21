5f5110348099ba6d434d5703722d84b0c7fda51f
jordonbigelow <the-music-dude@hotmail.com>
2025-09-20 20:52:11 -0600

    moved logic for responding to events to the game manager from the player to reduce coupling

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -7,17 +7,13 @@ const DOUBLE_JUMP_AMOUNT = 1
 var player_jump_amount = 0
 var can_player_double_jump: bool = true
 var kill_count: int = 0
-var heart_count: int = 3
 
 @onready var starting_scene: PackedScene = load("res://scenes/starting_screen.tscn")
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var player_sprite: Sprite2D = $Sprite2D
 @onready var health_hud_component := $Camera2D/HUD/HealthComponent
-@onready var respawn_timer: Timer = $RespawnTimer
 @onready var kill_count_component := $Camera2D/HUD/KillCountComponent
-@onready var heart_count_label := $Camera2D/HUD/HealthComponent/HeartCount
 @onready var death_message: Label = $Camera2D/HUD/DeadLabel
-
 @onready var player_won_message: Label = $Camera2D/HUD/YouWinLabel
 
 func _physics_process(delta: float) -> void:
@@ -63,8 +59,6 @@ func _physics_process(delta: float) -> void:
 func _on_kill_zone_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		death_message.visible = true
-		Engine.time_scale = 0.5
-		respawn_timer.start()
 
 
 func _on_respawn_timer_timeout() -> void:
@@ -75,16 +69,6 @@ func _on_respawn_timer_timeout() -> void:
 
 func _on_enemy_player_hit() -> void:
 	death_message.visible = true
-	
-	if heart_count > 0:
-		heart_count -= 1
-		health_hud_component.get_child(heart_count).hide()
-		heart_count_label.text = " x " + str(heart_count)
-	
-	if heart_count <= 0:
-		if respawn_timer.time_left <= 0:
-			Engine.time_scale = 0.25
-			respawn_timer.start()
 
 
 func _on_enemy_killed() -> void:
